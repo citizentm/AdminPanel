5c1bdb63c697b947a17a12de8231b8d2456959d4
Arpad Gabor <hello@arpadgabor.com>
2020-11-22 12:10:26 +0200

    fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,8 +16,5 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'max-len': {
-      code: 100,
-    },
   },
 }
